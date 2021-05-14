43c5bdc79a9e59ef49f68b79faba9b1d97cc0df1
Osama Khan <Osama6756@gmail.com>
2021-05-14 18:03:23 +0500

    Fixed Product DTO return value

--- src/products/products.dto.ts
+++ src/products/products.dto.ts
@@ -10,7 +10,16 @@ export class ProductRO {
    */
   static generate(input: Product | Product[]): any {
     const products: Product[] = [].concat(input);
-    const p = products.map(p => {p.title, p.description, p.rating, p.stock, p.img, p.category, p.updatedAt, p.createdAt});
+    const p = products.map(p => ({
+      title: p.title, 
+      description: p.description,
+      rating: p.rating, 
+      stock: p.stock, 
+      category: p.category, 
+      img: p.img, 
+      updatedAt: p.updatedAt, 
+      createdAt: p.createdAt
+    }));
     return p.length === 1? p[0]: p;
   }
 }

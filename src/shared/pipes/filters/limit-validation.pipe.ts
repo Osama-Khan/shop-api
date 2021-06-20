1ca75ac0c8d9760e085a2139a37108719898e2ac
Osama Khan <Osama6756@gmail.com>
2021-06-20 19:26:26 +0500

    Added check for limit to be greater than 0

--- src/shared/pipes/filters/limit-validation.pipe.ts
+++ src/shared/pipes/filters/limit-validation.pipe.ts
@@ -5,6 +5,7 @@ export class LimitValidationPipe implements PipeTransform {
     if (limit) {
       try {
         const l = parseInt(limit);
+        if (l <= 0) throw new Error();
         if (l.toString() !== limit) throw new Error();
         limit = l;
       } catch (e) {

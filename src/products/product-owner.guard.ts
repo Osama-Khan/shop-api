5ea5e007e704fc6cfbfa5e186ff0602457f40125
Osama Khan <Osama6756@gmail.com>
2021-07-02 08:49:24 +0500

    Added IsProductOwnerGuard for product ownership verification

---
+++ src/products/product-owner.guard.ts
@@ -0,0 +1,11 @@
+import { forwardRef, Inject } from '@nestjs/common';
+import IsOwnerGuard from 'src/shared/guards/is-owner.guard';
+import { ProductsService } from './products.service';
+
+export default class IsProductOwnerGuard extends IsOwnerGuard<ProductsService> {
+  constructor(
+    @Inject(forwardRef(() => ProductsService)) service: ProductsService,
+  ) {
+    super(service, 'product');
+  }
+}

5228ac6493c2b22781051bf809d982a48e99e5c1
Osama Khan <Osama6756@gmail.com>
2021-09-29 17:55:17 +0500

    Removed Connection entity from EventModule

--- src/chat/event/event.module.ts
+++ src/chat/event/event.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Connection } from '../repository/connection/connection.entity';
 import { Message } from '../repository/message/message.entity';
 import { EventService } from './event.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Connection, Message])],
+  imports: [TypeOrmModule.forFeature([Message])],
   providers: [EventService],
 })
 export class EventModule {}

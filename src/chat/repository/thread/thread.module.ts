2d2d918d54cfa4b5707769530b1e7c3139674ddf
Osama Khan <Osama6756@gmail.com>
2021-09-30 11:27:03 +0500

    Improved getThreadsOf endpoint to provide better response

--- src/chat/repository/thread/thread.module.ts
+++ src/chat/repository/thread/thread.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Thread } from './thread.entity';
 import { ThreadController } from './thread.controller';
+import { Message } from '../message/message.entity';
 import { ThreadService } from './thread.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Thread])],
+  imports: [TypeOrmModule.forFeature([Message, Thread])],
   controllers: [ThreadController],
   providers: [ThreadService],
 })
--- src/chat/repository/thread/thread.service.ts
+++ src/chat/repository/thread/thread.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import FindManyOptionsDTO from 'src/shared/models/find-many-options.dto';
 import { ApiService } from 'src/shared/services/api.service';
 import { Repository } from 'typeorm';
+import { Message } from '../message/message.entity';
 import { Thread } from './thread.entity';
 
 @Injectable()
@@ -10,12 +11,30 @@ export class ThreadService extends ApiService<Thread> {
   constructor(
     @InjectRepository(Thread)
     threadRepository: Repository<Thread>,
+    @InjectRepository(Message)
+    private messageRepository: Repository<Message>,
   ) {
     super(threadRepository, Thread.relations);
   }
 
-  /** Gets threads where the given user is a participant */
-  getThreadsOf(id: number, options: FindManyOptionsDTO<Thread>) {
-    return this.findAll({ ...options, where: [{ from: id }, { to: id }] });
+  /** Gets threads where the given user is a participant. If messages are not requested, returns
+   * `latestMessage` with the latest message in the thread instead of `messages` array
+   */
+  async getThreadsOf(id: number, options: FindManyOptionsDTO<Thread>) {
+    const where = [{ from: id }, { to: id }];
+    if (options?.relations?.includes('messages')) {
+      return this.findAll({ ...options, where });
+    }
+    const threads = await this.findAll({ ...options, where });
+    threads.data = await Promise.all(
+      threads.data.map(async (t) => {
+        t.latestMessage = await this.messageRepository.findOne({
+          relations: ['sender'],
+          where: { thread: t.id },
+        });
+        return t;
+      }),
+    );
+    return threads;
   }
 }

2f08ad918f1a08d67141af2410edaff0646f736e
Osama Khan <Osama6756@gmail.com>
2021-10-02 20:56:33 +0500

    Added EventService initialization to bootstrap method

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { EventService } from './chat/event/event.service';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  const nestServer = await app.listen(process.env.PORT);
+  app.get(EventService).initServer(nestServer);
 }
 bootstrap();

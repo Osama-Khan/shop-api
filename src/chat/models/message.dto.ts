d9f0a0e87c05e57416ebb1a1f3c65066af9fc4ea
Osama Khan <Osama6756@gmail.com>
2021-10-02 13:17:10 +0500

    Added threadId to messageDTO

--- src/chat/models/message.dto.ts
+++ src/chat/models/message.dto.ts
@@ -5,6 +5,8 @@ interface IMessageDTO {
   sender: number;
   /** Time the message was received at the server on */
   time: Date;
+  /** ID of the thread the message belongs to */
+  threadId: number;
 }
 
 /** DTO for the message object sent to the client */
@@ -12,10 +14,12 @@ export default class MessageDTO implements IMessageDTO {
   message: string;
   sender: number;
   time: Date;
+  threadId: number;
 
   constructor(message: IMessageDTO) {
     this.message = message.message;
     this.sender = message.sender;
     this.time = message.time;
+    this.threadId = message.threadId;
   }
 }

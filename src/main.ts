14c64c8074d2dd95fa70f9e617ca2a332958646c
Osama Khan <Osama6756@gmail.com>
2021-10-02 20:56:38 +0500

    Added CORS

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { EventService } from './chat/event/event.service';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const nestServer = await app.listen(process.env.PORT);
   app.get(EventService).initServer(nestServer);
 }

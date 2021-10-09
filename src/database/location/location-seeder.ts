2678956115e5a4ed23bcdebdb447c805ac6cd2ef
Osama Khan <Osama6756@gmail.com>
2021-10-09 15:06:20 +0500

    Added some optimizations to LocationSeeder
    - Seeding skipped if records are of the same length
    - Reduced number of logs to speed up seeding

--- src/database/location/location-seeder.ts
+++ src/database/location/location-seeder.ts
@@ -9,47 +9,47 @@ import { CityService } from 'src/api/location/city/city.service';
 
 async function seedCountries(app: INestApplication) {
   const svc = app.get(CountryService);
+  if ((await svc.count()) === countries.length) return;
+  LogHelper.info('Seeding countries...');
   for (const d of countries) {
     const c: any = { ...d, phoneCode: d.phone_code, subRegion: d.subregion };
-    LogHelper.info('[Country Seeder] - Inserting Country: ' + c.name);
     try {
       await svc.insert(c);
-      LogHelper.success('[Country Seeder] - Inserted Country: ' + c.name);
     } catch (ex) {
-      LogHelper.error('[Country Seeder] - Failed to insert Country: ' + c.name);
       throw ex;
     }
   }
+  LogHelper.success('Seeded countries!');
 }
 
 async function seedStates(app: INestApplication) {
   const svc = app.get(StateService);
+  if ((await svc.count()) === states.length) return;
+  LogHelper.info('Seeding states...');
   for (const d of states) {
     const s: any = { ...d, stateCode: d.state_code, country: d.country_id };
-    LogHelper.info('[State Seeder] - Inserting State: ' + s.name);
     try {
       await svc.insert(s);
-      LogHelper.success('[State Seeder] - Inserted State: ' + s.name);
     } catch (ex) {
-      LogHelper.error('[State Seeder] - Failed to insert State: ' + s.name);
       throw ex;
     }
   }
+  LogHelper.success('Seeded states!');
 }
 
 async function seedCities(app: INestApplication) {
   const svc = app.get(CityService);
+  if ((await svc.count()) === cities.length) return;
+  LogHelper.info('Seeding cities...');
   for (const d of cities as any) {
     const c: any = { ...d, state: d.state_id };
-    LogHelper.info('[City Seeder] - Inserting City: ' + c.name);
     try {
       await svc.insert(c as any);
-      LogHelper.success('[City Seeder] - Inserted City: ' + c.name);
     } catch (ex) {
-      LogHelper.error('[City Seeder] - Failed to insert City: ' + c.name);
       throw ex;
     }
   }
+  LogHelper.success('Seeded cities!');
 }
 
 export default async function seedLocations(app: INestApplication) {

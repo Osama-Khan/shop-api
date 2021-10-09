dc94fc9311c740f107996e0881b9c1f470e9162f
Osama Khan <Osama6756@gmail.com>
2021-10-09 15:04:55 +0500

    Fixed AppModule import

--- src/database/seeder.ts
+++ src/database/seeder.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { ApiModule } from 'src/api/api.module';
+import { AppModule } from 'src/app.module';
 import seedLocations from './location/location-seeder';
 
 async function seed() {
-  const app = await NestFactory.create(ApiModule);
+  const app = await NestFactory.create(AppModule);
   await seedLocations(app);
   app.close();
 }

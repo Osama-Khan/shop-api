af000dd324ad4ff120e33b11066202abca955c9e
Osama Khan <Osama6756@gmail.com>
2021-10-18 14:31:56 +0500

    Added REQUEST_LIMIT to control size limit of requests

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { json, urlencoded } from 'body-parser';
 import { EventService } from './chat/event/event.service';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: process.env.REQUEST_LIMIT }));
+  app.use(urlencoded({ limit: process.env.REQUEST_LIMIT, extended: true }));
   app.enableCors();
   const nestServer = await app.listen(process.env.PORT);
   app.get(EventService).initServer(nestServer);
